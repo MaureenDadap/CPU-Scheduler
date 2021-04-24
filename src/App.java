a039dc8efd12767eec59acd0939cd8fa4d33ace9
MaureenDadap <maureendadap@gmail.com>
2021-04-24 23:20:37 +0800

    menu text

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        char choice;
+
+        // -------
+        // Menu
+        // -------
+        do {
+            System.out.println("CHOOSE THE SCHEDULING ALGORITHM\n");
+            System.out.println("( 1 ) First-Come First-Served (FCFS) non-preemptive\n");
+            System.out.println("( 2 ) Shortest Job First (SJF) non-preemptive\n");
+            System.out.println("( 3 ) Shortest Remaining Time (SRT) preemptive\n");
+            System.out.println("( 4 ) Round-Robin (RR) preemptive\n");
+            System.out.println("( 5 ) Priority - non-preemptive\n");
+            System.out.println("( 6 ) Priority - preemptive\n");
+            System.out.println("( 0 ) EXIT\n");
+            System.out.println("CHOICE: ");
+
+            choice = scanner.next().charAt(0);
+
+        } while (choice != '0');
     }
 }
