14e5732e01fd959afbc140cd0d21eae2e822e64b
MaureenDadap <maureendadap@gmail.com>
2021-05-04 09:03:53 +0800

    created SRT algorithm but there is not much details yet for ex: when the  process came in/out of the queue

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -67,16 +67,6 @@ public class Algorithms {
             list.get(i).getValue()
                     .setTurnAroundTime(list.get(i).getValue().getBurst() + list.get(i).getValue().getWaitingTime());
         }
-
-        // CREATE THE TABLES
-        Utils.createTableSummary(list, false);
-        Utils.createGanttChart(list);
-
-        // PRINT AVERAGES
-        System.out.println("# Average Waiting Time: " + Utils.findAverageWT(list));
-        System.out.println("# Average Turnaround Time: " + Utils.findAverageTaT(list));
-
-        System.out.println("\n/////////////////////////////////////////////");
     }
 
     /**
@@ -152,33 +142,80 @@ public class Algorithms {
                 return temp;
             }
         });
-
-        // CREATE THE TABLES
-        Utils.createTableSummary(list, false);
-        Utils.createGanttChart(list);
-
-        // PRINT AVERAGES
-        System.out.println("# Average Waiting Time: " + Utils.findAverageWT(list));
-        System.out.println("# Average Turnaround Time: " + Utils.findAverageTaT(list));
-
-        System.out.println("\n/////////////////////////////////////////////");
     }
 
     /**
      * Shortest Remaining Time First Algorithm
      */
-    public static void shortestJobFirst(ProcessesTable processesTable) {
+    public static void shortestRemainingTime(ProcessesTable processesTable) {
         List<Map.Entry<String, ProcessesTable.TimeValues>> list = new ArrayList<Map.Entry<String, ProcessesTable.TimeValues>>(
                 processesTable.processesMap.entrySet());
 
-        // #1 - sort the processes according to arrival time
+        List<Map.Entry<String, ProcessesTable.TimeValues>> newList = new ArrayList<Map.Entry<String, ProcessesTable.TimeValues>>();
+        List<Boolean> isCompleted = new ArrayList<Boolean>();
+        List<Integer> remainingTime = new ArrayList<Integer>();
+        int completed = 0;
+        int currentTime = 0;
+        ProcessesTable.TimeValues values;
+
+        for (int i = 0; i < list.size(); i++) {
+            isCompleted.add(false);
+            remainingTime.add(list.get(i).getValue().getBurst());
+        }
+
+        while (completed != list.size()) {
+
+            int index = -1;
+            int min = 10000000;
+
+            for (int i = 0; i < list.size(); i++) {
+                values = list.get(i).getValue();
+
+                if (values.getArrival() <= currentTime && isCompleted.get(i) == false) {
+                    if (remainingTime.get(i) < min) {
+                        min = remainingTime.get(i);
+                        index = i;
+                    }
+                    if (remainingTime.get(i) == min) {
+                        if (values.getArrival() < list.get(index).getValue().getArrival()) {
+                            min = remainingTime.get(i);
+                            index = i;
+                        }
+                    }
+                }
+            }
+
+            if (index != -1) {
+                values = list.get(index).getValue();
+
+                if (remainingTime.get(index) == values.getBurst()) {
+                    values.setStartingTime(currentTime);
+                }
+
+                remainingTime.set(index, remainingTime.get(index) - 1);
+                currentTime++;
+
+                if (remainingTime.get(index) == 0) {
+                    values.setCompletionTime(currentTime);
+                    values.setTurnAroundTime(values.getCompletionTime() - values.getArrival());
+                    values.setWaitingTime(values.getTurnAroundTime() - values.getBurst());
+
+                    isCompleted.set(index, true);
+                    completed++;
+                }
+
+            } else {
+                currentTime++;
+            }
+        }
+
         Collections.sort(list, new Comparator<Map.Entry<String, ProcessesTable.TimeValues>>() {
             public int compare(Map.Entry<String, ProcessesTable.TimeValues> o1,
                     Map.Entry<String, ProcessesTable.TimeValues> o2) {
 
                 int temp;
-                int a = o1.getValue().getArrival();
-                int b = o2.getValue().getArrival();
+                int a = o1.getValue().getCompletionTime();
+                int b = o2.getValue().getCompletionTime();
 
                 if (a > b)
                     temp = +1;
@@ -190,22 +227,6 @@ public class Algorithms {
                 return temp;
             }
         });
-
-        // CALCULATE THE TIME VALUES
-        // Utils.findStartingTime(list);
-        // Utils.findCompletionTime(list);
-        // Utils.findWaitingTime(list);
-        // Utils.findTurnAroundTime(list);
-
-        // CREATE THE TABLES
-        Utils.createTableSummary(list, false);
-        Utils.createGanttChart(list);
-
-        // PRINT AVERAGES
-        // System.out.println("# Average Waiting Time: " + Utils.findAverageWT(list));
-        // System.out.println("# Average Turnaround Time: " + Utils.findAverageTaT(list));
-
-        System.out.println("\n/////////////////////////////////////////////");
     }
 
     /**
@@ -281,15 +302,175 @@ public class Algorithms {
                 return temp;
             }
         });
-
-        // CREATE THE TABLES
-        Utils.createTableSummary(list, true);
-        Utils.createGanttChart(list);
-
-        // PRINT AVERAGES
-        System.out.println("# Average Waiting Time: " + Utils.findAverageWT(list));
-        System.out.println("# Average Turnaround Time: " + Utils.findAverageTaT(list));
-
-        System.out.println("\n/////////////////////////////////////////////");
     }
+
+    /**
+     * (PREEMPTIVE) Priority Algorithm.
+     */
+    public static void prioritySchedulingPreemptive(ProcessesTable processesTable) {
+        List<Map.Entry<String, ProcessesTable.TimeValues>> list = new ArrayList<Map.Entry<String, ProcessesTable.TimeValues>>(
+                processesTable.processesMap.entrySet());
+
+        List<Boolean> isCompleted = new ArrayList<Boolean>();
+        int completed = 0;
+        int currentTime = 0;
+        ProcessesTable.TimeValues values;
+
+        for (int i = 0; i < list.size(); i++) {
+            isCompleted.add(false);
+        }
+
+        while (completed != list.size()) {
+
+            int index = -1;
+            int max = 1000;
+
+            for (int i = 0; i < list.size(); i++) {
+                values = list.get(i).getValue();
+
+                if (values.getArrival() <= currentTime && isCompleted.get(i) == false) {
+                    if (values.getPriority() < max) {
+                        max = values.getPriority();
+                        index = i;
+                    }
+                    if (values.getPriority() == max) {
+                        if (values.getArrival() < list.get(index).getValue().getArrival()) {
+                            max = values.getPriority();
+                            index = i;
+                        }
+                    }
+                }
+            }
+
+            if (index != -1) {
+                values = list.get(index).getValue();
+
+                values.setStartingTime(currentTime);
+                values.setCompletionTime(values.getStartingTime() + values.getBurst());
+                values.setTurnAroundTime(values.getCompletionTime() - values.getArrival());
+                values.setWaitingTime(values.getTurnAroundTime() - values.getBurst());
+
+                isCompleted.set(index, true);
+                completed++;
+                currentTime = values.getCompletionTime();
+
+            } else {
+                currentTime++;
+            }
+        }
+
+        Collections.sort(list, new Comparator<Map.Entry<String, ProcessesTable.TimeValues>>() {
+            public int compare(Map.Entry<String, ProcessesTable.TimeValues> o1,
+                    Map.Entry<String, ProcessesTable.TimeValues> o2) {
+
+                int temp;
+                int a = o1.getValue().getStartingTime();
+                int b = o2.getValue().getStartingTime();
+
+                if (a > b)
+                    temp = +1;
+                else if (a < b)
+                    temp = -1;
+                else
+                    temp = 0;
+
+                return temp;
+            }
+        });
+    }
+
+    /**
+     * Multi Level Queue Algorithm. FIXED PRIO
+     */
+    public static void multilevelQueue(ProcessesTable processesTable) {
+        List<Map.Entry<String, ProcessesTable.TimeValues>> list = new ArrayList<Map.Entry<String, ProcessesTable.TimeValues>>(
+                processesTable.processesMap.entrySet());
+
+        ProcessesTable q1 = new ProcessesTable();
+        ProcessesTable q2 = new ProcessesTable();
+
+        List<Boolean> lvl1isCompleted = new ArrayList<Boolean>();
+        List<Boolean> lvl2isCompleted = new ArrayList<Boolean>();
+
+        int completed1 = 0;
+        int completed2 = 0;
+
+        // for (int i = 0; i < list.size(); i++) {
+        //     if (list.get(i).getValue().getLevel() == 1)
+        //         lvl1isCompleted.add(false);
+        //     else
+        //         lvl2isCompleted.add(false);
+        // }
+
+        // Collections.sort(list, new Comparator<Map.Entry<String, ProcessesTable.TimeValues>>() {
+        //     public int compare(Map.Entry<String, ProcessesTable.TimeValues> o1,
+        //             Map.Entry<String, ProcessesTable.TimeValues> o2) {
+
+        //         int temp;
+        //         int a = o1.getValue().getLevel();
+        //         int b = o2.getValue().getLevel();
+
+        //         if (a > b)
+        //             temp = +1;
+        //         else if (a < b)
+        //             temp = -1;
+        //         else
+        //             temp = 0;
+
+        //         return temp;
+        //     }
+        // });
+
+        // List<Map.Entry<String, ProcessesTable.TimeValues>> q1 = new
+        // ArrayList<Map.Entry<String, ProcessesTable.TimeValues>>();
+        // List<Map.Entry<String, ProcessesTable.TimeValues>> q2 = new
+        // ArrayList<Map.Entry<String, ProcessesTable.TimeValues>>();
+        // List<Boolean> isCompleted = new ArrayList<Boolean>();
+
+        // for (int i = 0; i < list.size(); i++) {
+        // ProcessesTable.TimeValues values = list.get(i).getValue();
+        // ProcessesTable.TimeValues values2;
+        // int arrival = values.getArrival();
+        // int burst = values.getBurst();
+        // int priority = values.getPriority();
+        // int level = values.getLevel();
+
+        // if (list.get(i).getValue().getLevel() == 1) {
+        // values2 = q1.new TimeValues(arrival, burst, 0, 0, priority, 0, 0, level);
+        // q1.processesMap.put(list.get(i).getKey(), values2);
+        // } else {
+        // values2 = q2.new TimeValues(arrival, burst, 0, 0, priority, 0, 0, level);
+        // q2.processesMap.put(list.get(i).getKey(), values2);
+        // }
+        // }
+
+        // // queue level 1 uses fcfs
+        // firstComeFirstServe(q1);
+        // // queue level 2 uses priority
+        // priorityScheduling(q2);
+
+        // level 1 queue uses fcfs
+        while (completed1 != lvl1isCompleted.size()) {
+        }
+
+        Collections.sort(list, new Comparator<Map.Entry<String, ProcessesTable.TimeValues>>() {
+            public int compare(Map.Entry<String, ProcessesTable.TimeValues> o1,
+                    Map.Entry<String, ProcessesTable.TimeValues> o2) {
+
+                int temp;
+                int a = o1.getValue().getCompletionTime();
+                int b = o2.getValue().getCompletionTime();
+
+                if (a > b)
+                    temp = +1;
+                else if (a < b)
+                    temp = -1;
+                else
+                    temp = 0;
+
+                return temp;
+            }
+        });
+    }
+
 }
